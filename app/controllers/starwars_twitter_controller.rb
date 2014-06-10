e6b462da97e50e47f9170c3a89ba92ac016fd1ee
mikemjharris <mike.mj.harris@gmail.com>
2014-06-10 08:27:22 +0100

    added titile to landing page

--- app/controllers/starwars_twitter_controller.rb
+++ app/controllers/starwars_twitter_controller.rb
@@ -31,14 +31,21 @@ class StarwarsTwitterController < ApplicationController
       flash[:nouser] = "No user that name by. Do. Or not do. There is no try"
       redirect_to :root
     else
+    # link text
+        
 
     @tweets = client.user_timeline(params[:id], {count: 20})
     @name = @tweets[0].user.name
     @user_image = @tweets[0].user.profile_image_url
-     
-     
+    if current_user.twittername = @tweets[0].user.nickname 
+          @text = ["Check out my star wars intro twitter feed!", "My timeline starwarsified!"].sample
+    else
+        @text = ["Checkout " + @name + "'s star wars twitter feed"]    
+    end
     render layout: 'basic'
     end 
   end
+
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:twitter, :google_oauth2, :facebook, :github, :linkedin, :instagram]
+         :omniauthable, omniauth_providers: [:twitter,  :facebook]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :twittername, :password, :password_confirmation, :remember_me, :uid, :provider, :image, :facebook_token, :twitter_token, :github_token, :instagram_token, :linkedin_token, :google_oauth2_token 
+  attr_accessible :email, :twittername, :password, :password_confirmation, :remember_me, :uid, :provider, :image 
   # attr_accessible :title, :body
 
   has_many :collagephotos
@@ -14,22 +14,19 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     
     twitter_email = if auth.provider =="twitter" then auth.info.nickname.downcase + "@twitter.com" end
-    instagram_email = if auth.provider =="instagram" then auth.info.nickname.downcase + "@instagram.com" end
     
-    if user = User.find_by_email(auth.info.email) || user = User.find_by_email(twitter_email) || user = User.find_by_email(instagram_email) 
+    
+    if user = User.find_by_email(auth.info.email) || user = User.find_by_email(twitter_email) 
       user.provider = auth.provider
       user.uid = auth.uid
       user.image = auth.info.image
       if auth.provider == "facebook"
         user.facebook_token = auth.credentials.token
-      else
-        user.instagram_token = auth.credentials.token
       end
       user
     else
       
       if auth.provider == "twitter"
-
           user = User.new()
           user.provider = auth.provider
           user.uid = auth.uid
@@ -40,16 +37,6 @@ class User < ActiveRecord::Base
           user.save
           user
 
-      elsif auth.provider == "instagram"
-          test = User.create({
-              :provider => auth.provider,
-              :uid => auth.uid,
-              :email => auth.info.nickname.downcase + "@instagram.com",
-              :image => auth.info.image,
-              :password => Devise.friendly_token[0,20],
-              :instagram_token => auth.credentials.token
-          })
-          
       else
 
         where(auth.slice(:provider, :uid)).first_or_create do |user|
@@ -65,16 +52,4 @@ class User < ActiveRecord::Base
   end
 end
 
-    # User.create({
-    #   :provider => auth.provider,
-    #   :uid => auth.uid,
-    #   :email => auth.info.email,
-    #   :password => Devise.friendly_token[0,20]
-    # })
-    
-
-      
-      # user.uid = auth.uid
-      # user.email = auth.info.email
-      # user.password = Devise.friendly_token[0,20]
-    
+  
