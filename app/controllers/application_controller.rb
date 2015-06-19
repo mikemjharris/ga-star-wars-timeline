8be31bb9efb690af0136336662673ba1e041bf8d
Mike Harris <mike.mj.harris@gmail.com>
2015-06-19 18:22:00 +0100

    Remove unnecessary code and old files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-    
-
-  def after_sign_in_path_for(user)
-
-    starwars_twitter_path(user.twittername)
-  end
-
-  def after_sign_out_path_for(user)
-    root_path
-  end
-
-  
-
- 
 end
