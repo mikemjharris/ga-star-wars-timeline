6333c24d2e4744a62bcc4e01bc1d3368f9632318
Mike Harris <mike.mj.harris@gmail.com>
2015-06-19 18:18:19 +0100

    Rmeove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,19 @@
 Contacts::Application.routes.draw do
 
-  devise_for :users, controllers: {omniauth_callbacks: 'omniauth_callbacks'}
 
-  
-  
 
   resources :contacts
   resources :collages
   resources :starwars_facebook
   resources :starwars_twitter
 
-    get 'apicalls/instagram', to:  'apicalls#instagram', as: 'instagram_api'
-    get 'apicalls/facebook', to:  'apicalls#facebook', as: 'facebook_api'
-    get 'apicalls/facebook_auth', to:  'apicalls#facebook_auth', as: 'facebook_auth'
-    get 'apicalls/game', to:  'apicalls#game', as: 'game'
-    get 'apicalls/hacks', to:  'apicalls#hacks', as: 'hacks'
-    get 'contacts/facebookwars', to:  'contacts#facebookwars', as: 'facebookwars'
-    
-    
 
   root to: 'starwars_twitter#index'
-    
-   
-  
 
 
- 
+
+
+
+
 
 end
